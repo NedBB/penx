9dc94ff18852f61d8d69ab2725e1cf8a05861b3c
Chinedu Onuegbu <neduidea@gmail.com>
2024-05-24 10:39:47 +0100

    update bank model and staff payment schedule

--- resources/views/livewire/queries/staff-schedule.blade.php
+++ resources/views/livewire/queries/staff-schedule.blade.php
@@ -17,10 +17,10 @@
                         <div class="col-md-3">
                             <div class="table-responsive text-nowrap"> 
                               <div class="dt-buttons">
-                                <a href="#" onclick="extractContentForPrinting(6,'staff-schedule')"  id="print" class="dt-button buttons-collection dropdown-toggle btn btn-label-primary me-2" tabindex="0" aria-controls="DataTables_Table_0" type="button" aria-haspopup="dialog" aria-expanded="false">    
+                                {{-- <a href="#" onclick="extractContentForPrinting(6,'staff-schedule')"  id="print" class="dt-button buttons-collection dropdown-toggle btn btn-label-primary me-2" tabindex="0" aria-controls="DataTables_Table_0" type="button" aria-haspopup="dialog" aria-expanded="false">    
                                   <span><i class="ti ti-file-export me-sm-1"></i> </span>
                                   <span class="d-none d-sm-inline-block">Print</span>
-                                </a>
+                                </a> --}}
                                 <x-export-excell />
                               </div>
                             </div>
